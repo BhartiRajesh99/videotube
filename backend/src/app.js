120a58816221c11444c8f02b38035c197716a17c
Rajesh <rajeshbharti1110@gmail.com>
2026-01-22 15:42:27 +0530

    update cors config

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,8 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
@@ -19,26 +21,26 @@ app.use(cookieParser());
 // import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
-import commentRouter from "./routes/comment.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
+import commentRouter from "./routes/comment.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 import { errorHandler } from "./middlewares/error.middlewares.js";
 
 // routes
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/comment", commentRouter)
-app.use("/api/v1/video", videoRouter)
-app.use("/api/v1/like", likeRouter)
-app.use("/api/v1/tweet", tweetRouter)
-app.use("/api/v1/subscription", subscriptionRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/playlist", playlistRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 export { app };
